2e8f86a1758e6db4b4f234ae19485779522fe767
Zohad2307 <adel1086@stud.kea.dk>
2021-09-29 09:04:30 +0200

    "first commit"

---
+++ src/A.java
@@ -0,0 +1,10 @@
+public class A {
+        private int a = 100;
+        public void setA( int value) {
+            this.a = value; }
+        public int getA() {
+            return a;
+        }
+
+    //class A
+}
